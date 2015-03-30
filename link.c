048447dda884491118d812b7c4e666e5a0670470
Hualiang Li <hualiang@hwaii.edu>
2015-03-29 16:54:22 -1000

    link.c modified.

--- link.c
+++ link.c
@@ -1,43 +1,38 @@
-/* 
+/*  
+ *  Functions for the link
+ *
  *  The functions linkSend and linkReceive will send and receive
  *  packets on links (e.g., pipes).  This is currently implemented
  *  by converting the packet information into a string of bytes,
  *  where each field in the packet (i.e., source address, destination
  *  addres, length, payload) is separated by spaces.  In other words,
- *  spaces are used to delimit the fields.  (By the way, this isn't
- *  the best way to implement.  This is a result of using the string.h 
- *  libary to do operations on strings.)
+ *  spaces are used to delimit the fields.  The addresses
+ *  and length are integers and they are converted into ascii text
+ *  before putting them on the pipe.  They payload can be arbitrary
+ *  bytes including spaces and '\0'.  So we need to remove these
+ *  before transmission. Otherwise the receiving side of the link
+ *  will unable to tell if a byte is a delimiter or part of the
+ *  payload.
  *
- *  A problem of this approach is that the payload is restricted
- *  from having the 0x20 byte in the data sent in a packet
- *  because 0x20 is the ASCII byte for the space character.  
- *  In order to ensure that the payload can carry any string of 
- *  bytes, the payload data is preprocessed to convert each data 
- *  byte into two ASCII characters.  The conversion is as follows.
+ *  We use the following simple rule to convert
+ *  these bytes into two bytes:
  *
- *  A data byte is 8 bits: b7 b6 ... b0.  The bits are divided
- *  into two 4-bit chunks called "nibbles".  The two nibbles are
- *  (b7 b6 b5 b4) and (b3 b2 b1 b0).  The high-order
- *  nibble (b7 ... b4) can be encoded into an ASCII alphabet.
- *  In particular, the bits 0000, 0001, 0010,..., 1111
- *  can be encoded to alphabets 'a', 'b', ... 'p', respectively.
- *  To encode a nibble N to an alphabet A, we use the 
- *  operation A = N + 'a'.  Similar conversion can be done for 
- *  the low order nibble.  
- * 
- *  Thus, this process will encode a string of data bytes into a string of
- *  ASCII characters from the set 'a', 'b', ..., 'p'.  (Note
- *  that this string is twice as long as the data string.)
- *  Other fields of the packet such as the length and addrsses can
- *  be encoded too. 
+ *  ' '  -> *A
+ *  '\0' -> *B
+ *  '*'  -> *C
  *
- *  Note that these encoded strings can be separated by spaces. 
+ *  For example, suppose the payload is "*Hello world*\n".  Then
+ *  the conversion is "*CHello*Aworld*C".   We've eliminated all
+ *  the spaces from the payload.
+ *  
+ *  Note that in addition to ' ', we are also converting '\0' and '*'.
+ *  In the case of '*', we are using it as a control character. 
+ *  So there is ambiguity about whether it is for control or data.
+ *  In the case of '\0', it is a control character for string.h
+ *  operations.
  *
- *  The encoded ASCII strings can be put in the pipe.  
- *
- *  On the receiving side of the pipe, the encoded strings are
- *  decoded to get the original values.  The decoding process does
- *  the opposite of the encoding.
+ *  Now to recover the original payload, the receiver just does
+ *  the opposite of the conversion.
  */
 
 #include <stdio.h>
@@ -53,6 +48,7 @@
 
 #define PIPEREAD  0
 #define PIPEWRITE 1
+//#define DEBUG
 
 /* Closes a link */
 int linkClear(LinkInfo * link)
@@ -102,8 +98,6 @@ char word[1000];
 int count;
 int k;
 int wordptr;
-char lowbits;
-char highbits;
 
 n = 0;
 
@@ -114,8 +108,8 @@ if (link->linkType==UNIPIPE) {
        * Something is received on link. 
        * Store it in the packet buffer
        */
-
       buffer[n] = '\0';
+//    printf("DEBUG: Link received=%s\n",buffer);
    
       findWord(word, buffer, 1); /* Destination address */
       pbuff->dstaddr = ascii2Int(word);
@@ -131,19 +125,23 @@ if (link->linkType==UNIPIPE) {
       /* 
        * We will transform the payload so that 
        *
-       *  Each symbol 'a', 'b', ..., 'p' converts to the 
-       *  4-bits 0000, 0001,..., 1111
-       *  Each pair of symbols converts to a byte.
-       *  For example, 'ac' converts to 00000010
-       *  Note the first symbol is the high order bits
-       *  and the second symbol is the low order bits
+       * *A -> ' '
+       * *B -> '\0' 
+       * *C -> '*'
        */
 
-      for (k = 0; k < pbuff->length; k++){
-         highbits = word[2*k]-'a';  
-         lowbits = word[2*k+1]-'a';
-         highbits = highbits * 16; /* Shift to the left by 4 bits */
-         pbuff->payload[k] = highbits + lowbits;
+      k = 0;
+      wordptr=0;
+      for (count=pbuff->length; count>0; count--) {
+         if (word[wordptr]=='*') {
+            wordptr++;
+            if (word[wordptr]=='A') pbuff->payload[k] = ' ';
+            else if (word[wordptr]=='B') pbuff->payload[k] = '\0';
+            else if (word[wordptr]=='C') pbuff->payload[k] = '*';
+         }
+         else pbuff->payload[k] = word[wordptr];
+         wordptr++;
+         k++;
       } /* end of for */
       pbuff->payload[k] = '\0';
       pbuff->valid=1;
@@ -171,9 +169,10 @@ int  count;
 int  sendbuffptr;
 int  newptr;
 int  k;
-char lowbits;
-char highbits;
 
+#ifdef DEBUG
+printf("Entering linksend\n");
+#endif
 /* Check if this send should be aborted */
 if (pbuff->valid == 0) {
    printf("packet invalid\n");
@@ -190,8 +189,16 @@ if (pbuff->length <= 0) {
    return -1;
 }
 
+#ifdef DEBUG
+printf("After If statements\n");
+#endif
+
 sendbuff[0] = ' ';  /* Start message with a space */
 sendbuff[1] = '\0';
+
+#ifdef DEBUG
+printf("After sendbuff statements\n");
+#endif
 
 int2Ascii(word, pbuff->dstaddr);  /* Append destination address */
 appendWithSpace(sendbuff, word);
@@ -202,30 +209,36 @@ appendWithSpace(sendbuff, word);
 int2Ascii(word, pbuff->length);  /* Append payload length */
 appendWithSpace(sendbuff, word);
 
-/* 
- * We will transform the payload so that 
- * a byte will be converted into two
- * ASCII symbols, each symbol represents
- * four bits. The ASCII symbols are 
- * 'a', 'b', ..., 'p' representing
- * 0000, 0001, 0010, ..., 1111  
- * For example, the byte 00000010 will
- * be represented by 'ab'.  Note that
- * the first byte is the high order bits
- * and the second byte is the low order bits.
+/* We will transform the payload so that 
+ *
+ * ' '   -> *A
+ * '\0'  -> *B
+ * '*'   -> *C
  */
 
-for (k = 0; k < pbuff->length; k++) {
-   lowbits = pbuff->payload[k];
-   highbits = lowbits;
-   highbits = highbits/16; /* shift bits down by four bits */
-   highbits = highbits & 15; /* Mask out all bits except the last four */
-   lowbits = lowbits & 15;
-   newpayload[2*k] = highbits + 'a';
-   newpayload[2*k+1] = lowbits + 'a'; 
+k = 0;
+newptr = 0;
+
+for (count=pbuff->payload[1]+2; count>0; count--) {
+   if (pbuff->payload[k] == ' ') {
+      newpayload[newptr++] = '*';
+      newpayload[newptr++] = 'A';
+   }
+   else if (pbuff->payload[k] == '\0') {
+      newpayload[newptr++] = '*';
+      newpayload[newptr++] = 'B';
+   } 
+   else if (pbuff->payload[k] == '*') {
+      newpayload[newptr++] = '*';
+      newpayload[newptr++] = 'C';
+   } 
+   else {
+      newpayload[newptr++] = pbuff->payload[k];
+   } 
+   k++;
 }
 
-newpayload[2*k] = '\0';
+newpayload[newptr] = '\0';
 
 appendWithSpace(sendbuff, newpayload);
 
@@ -234,6 +247,6 @@ if (link->linkType==UNIPIPE) {
 }
 
 /* Used for DEBUG -- trace packets being sent */
-printf("Link %d transmitted\n",link->linkID);
+//printf("Link %d transmitted\n",link->linkID);
 }
 
